507a34da6f34e8ae060aca837cc8e35a693bed23
Eyob Addis <121459779+eyob-12@users.noreply.github.com>
2024-02-28 17:09:09 +0300

    UserContext.jsx

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,28 +1,20 @@
-import  { createContext, useEffect, useState } from "react";
-import PropTypes from "prop-types"; // Import PropTypes
+import {createContext, useEffect, useState} from "react";
 import axios from "axios";
 
 export const UserContext = createContext({});
 
-export function UserContextProvider({ children }) {
-    const [username, setUsername] = useState(null)
-    const [id, setId] = useState(null)
-
-    useEffect(() => {
-        axios.get('/profile').then(response => {
-            setId(response.data.userId);
-            setUsername(response.data.username);
-            
-        });
-    }, []);
-    return (
-        <UserContext.Provider value={{ username, setUsername, id, setId }}>
-            {children}
-        </UserContext.Provider>
-    );
+export function UserContextProvider({children}) {
+  const [username, setUsername] = useState(null);
+  const [id, setId] = useState(null);
+  useEffect(() => {
+    axios.get('/profile').then(response => {
+      setId(response.data.userId);
+      setUsername(response.data.username);
+    });
+  }, []);
+  return (
+    <UserContext.Provider value={{username, setUsername, id, setId}}>
+      {children}
+    </UserContext.Provider>
+  );
 }
-
-// Add prop types validation
-UserContextProvider.propTypes = {
-    children: PropTypes.node.isRequired, // Ensure children is a React node
-};
